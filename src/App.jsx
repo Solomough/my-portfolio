eca1d311f2ffb4a11696aa50972df4fd5cdeec59
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-09-24 22:53:12 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
+import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
+const App = () => {
+  return (
+    <div className="App scroll-smooth">
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
